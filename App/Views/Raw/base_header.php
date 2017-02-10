88dae9f77da130af30226fcf17b4b99c70173224
Jacob Silva <jacob.silva0123@gmail.com>
2017-02-10 00:41:35 -0800

    Fix Syntax Error

--- App/Views/Raw/base_header.php
+++ App/Views/Raw/base_header.php
@@ -2,7 +2,7 @@
     <html>
         <head>
             <meta charset="utf-8">
-            <title><?php echo htmlspecialchars(isset($title) ? $title : 'Bingo Framework';?></title>
+            <title><?php echo htmlspecialchars(isset($title) ? $title : 'Bingo Framework');?></title>
             <link rel="stylesheet" type="text/css" href="<?php echo htmlentities(isset($stylesheet) ? $stylesheet : '', ENT_QUOTES, 'UTF-8');?>">
             <link rel="stylesheet" type="text/css" href="<?php echo htmlentities(isset($font) ? $font : '', ENT_QUOTES, 'UTF-8');?>">
         </head>
